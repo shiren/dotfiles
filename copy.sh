8de803b98bbb21fe8fd37364e823b1bd89c2fa61
Sungho Kim <shirenbeat@gmail.com>
2015-05-29 22:09:07 +0900

    awef

--- copy.sh
+++ copy.sh
@@ -4,6 +4,7 @@ cp ~/mydevset/.zshrc ~/.zshrc
 cp ~/mydevset/.zprofile ~/.zprofile
 cp ~/mydevset/.ideavimrc ~/.ideavimrc
 cp ~/mydevset/.tmux.conf ~/.tmux.conf
+cp -R ~/mydevset/Settings.textexpandersettings ~/Library/Mobile\ Documents/com~apple~CloudDocs/textExpander/Settings.textexpandersettings
 
 cp ~/mydevset/fish/config.fish ~/.config/fish/config.fish 
 cp -rf ~/mydevset/fish/functions ~/.config/fish/functions
