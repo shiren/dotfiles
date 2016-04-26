c7ace2ed198fab19cdd64f725f96a8b80c26cfdb
Sungho Kim <shirenbeat@gmail.com>
2016-04-26 18:48:47 +0900

    emacs setup

---
+++ emacs.d/elpa/evil-escape-20160313.1205/evil-escape-pkg.el
@@ -0,0 +1,1 @@
+(define-package "evil-escape" "20160313.1205" "Escape from anything with a customizable key sequence" '((emacs "24") (evil "1.0.9") (cl-lib "0.5")) :url "https://github.com/syl20bnr/evil-escape" :keywords '("convenience" "editing" "evil"))
