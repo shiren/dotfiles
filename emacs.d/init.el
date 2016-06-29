44baf566a5494bfe3345b17524c779212c5dfecd
Sungho Kim <shirenbeat@gmail.com>
2016-06-29 10:23:19 +0900

    tmux prefix bind to C-j

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -30,6 +30,11 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
 
