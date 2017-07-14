8bb70abf17e0ed74caf985a4db4b88262ef6721c
Sungho Kim <shiren.coder@navercorp.com>
2017-07-14 10:50:42 +0900

    eyebrowsse-keymap-prefix changed

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -209,7 +209,7 @@
 (use-package eyebrowse
   :ensure t
   :init
-  (setq eyebrowse-keymap-prefix (kbd "C-c C-e"))  
+  (setq eyebrowse-keymap-prefix (kbd "C-c C-c"))
   (eyebrowse-mode t)
   :bind
   ("C-j ;" . eyebrowse-last-window-config)
