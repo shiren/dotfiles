a6fed1a90b904f316196cc5a00c44e4f8f639fe4
shiren <shirenbeat@gmail.com>
2017-08-08 22:05:38 +0900

    /Users/shiren/dotfiles/emacs.d/init.el update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -25,10 +25,10 @@
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
-(when (and window-system (eq system-type 'darwin))
-  (set-face-attribute 'default nil :family "Source Code Pro")
-  (set-face-attribute 'default nil :height 130 :weight 'ultralight)
-  (set-fontset-font t 'hangul (font-spec :name "나눔고딕")))
+;; (when (and window-system (eq system-type 'darwin))
+;;   (set-face-attribute 'default nil :family "Source Code Pro")
+;;   (set-face-attribute 'default nil :height 130 :weight 'ultralight)
+;;   (set-fontset-font t 'hangul (font-spec :name "나눔고딕")))
 
 (setq mac-command-key-is-meta t)
 (setq mac-command-modifier 'meta)
