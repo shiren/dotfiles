65b0279599e50aac797962e8fcf48731a546bff9
Sungho Kim <shiren@nhnent.com>
2017-08-30 12:26:27 +0900

    /Users/shiren/dotfiles/emacs.d/init.el update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -514,6 +514,8 @@
   :ensure t
   :init
   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
+  (add-to-list 'auto-mode-alist '("tui-gate\\components\\.js'" . rjsx-mode))
+
   :config
   (define-key rjsx-mode-map "<" nil)
   (define-key rjsx-mode-map (kbd "C-d") nil))
