f8760b7c03de400449a0bdf68d733e18873ef81b
shiren <shirenbeat@gmail.com>
2017-11-02 20:47:32 +0900

    init

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -379,7 +379,7 @@
 
 (use-package paredit
   :ensure t
-  :init
+  :config
   (define-key paredit-mode-map (kbd "C-j") nil)
   (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
   (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
