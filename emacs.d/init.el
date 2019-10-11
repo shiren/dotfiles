60084f6620b47f54b1a3340784d00d3c5b80f3bb
Sungho Kim <shirenbeat@gmail.com>
2019-10-11 11:16:10 +0900

    init: Adjust garbage collection size

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -32,6 +32,13 @@
 
 (setq tab-width 2)
 
+;; Adjust garbage collection thresholds during startup, and thereafter
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
 ;; 이맥스르 투명하게 하려면 숫자 조절
 (set-frame-parameter nil 'alpha 1.0)
 
