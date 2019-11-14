007dc456d7934fb6c2cb4846ac8649ddec6c35e9
Sungho Kim <shirenbeat@gmail.com>
2019-11-14 12:23:26 +0900

    init: vterm

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -807,7 +807,7 @@
   (interactive)
   (tide-setup)
   (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (setq flycheck-check-syntax-automatically '(save mode-enabled new-line))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
   (company-mode +1)
@@ -1135,7 +1135,7 @@
     (format-time-string fmt time)))
 
 (defun shiren-org-log-time-for-entry ()
-  (let ((start-ts (shiren-org-get-formatted-time-stamp org-clock-start-time))
+  (let ((start-ts (shiren-org-get-feormatted-time-stamp org-clock-start-time))
         (end-ts (shiren-org-get-formatted-time-stamp (float-time)))
         (today-datetree (format-time-string "%Y-%m-%d %A" (float-time))))
     (with-current-buffer (find-file-noselect "~/org/agenda/timelogs.org")
@@ -1162,6 +1162,14 @@
           (lambda ()
             (define-key term-raw-map (kbd "C-j")
               (lookup-key (current-global-map) (kbd "C-j")))))
+
+(use-package vterm
+  :ensure t)
+
+(use-package multi-libvterm
+  :ensure t
+  :init
+  :config)
 
 (defun auto-commit-files (list)
   (interactive
@@ -1272,5 +1280,5 @@
     ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "ef280e6d5105f7d3906ae43a40aff5490970337796cd5f8a53207568b7e784d0" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (zoom yasnippet-snippets wttrin writeroom-mode whitespace-cleanup-mode which-key wgrep web-mode vue-mode use-package-ensure-system-package use-package-chords tide swift-mode suggest spacemacs-theme spaceline shut-up rust-playground rjsx-mode rainbow-mode rainbow-delimiters racer prodigy prettier-js pocket-reader parinfer paredit ox-reveal ox-gfm org-tree-slide org-bullets ob-typescript ob-swift ob-rust ob-restclient ob-go nov multiple-cursors multi-term material-theme lsp-ui lsp-sourcekit lsp-javascript-typescript js-doc indent-guide iedit ibuffer-projectile hyperbole highlight-thing highlight-indent-guides helpful graphql goto-last-change google-translate git-timemachine git-gutter forge flycheck-swiftlint flycheck-swift flycheck-rust flycheck-package eyebrowse expand-region exec-path-from-shell evil-escape evil dumb-jump diminish delight dashboard counsel-projectile company-sourcekit company-lsp company-go cider cargo beacon ace-window))))
+    (multi-libvterm zoom yasnippet-snippets wttrin writeroom-mode whitespace-cleanup-mode which-key wgrep web-mode vue-mode use-package-ensure-system-package use-package-chords tide swift-mode suggest spacemacs-theme spaceline shut-up rust-playground rjsx-mode rainbow-mode rainbow-delimiters racer prodigy prettier-js pocket-reader parinfer paredit ox-reveal ox-gfm org-tree-slide org-bullets ob-typescript ob-swift ob-rust ob-restclient ob-go nov multiple-cursors multi-term material-theme lsp-ui lsp-sourcekit lsp-javascript-typescript js-doc indent-guide iedit ibuffer-projectile hyperbole highlight-thing highlight-indent-guides helpful graphql goto-last-change google-translate git-timemachine git-gutter forge flycheck-swiftlint flycheck-swift flycheck-rust flycheck-package eyebrowse expand-region exec-path-from-shell evil-escape evil dumb-jump diminish delight dashboard counsel-projectile company-sourcekit company-lsp company-go cider cargo beacon ace-window))))
 (put 'set-goal-column 'disabled nil)
