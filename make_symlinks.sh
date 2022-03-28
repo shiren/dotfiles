7fad3f3b9b25e5aa11c64c3a308e369c25eeb191
Sungho Kim(shiren) <shirenbeat@gmail.com>
2022-03-28 18:23:06 +0900

    neovim

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc tmux.conf ideavimrc zprofile emacs.d/init.el"    # list of files/folders to symlink in homedir
+files="vimrc zshrc tmux.conf ideavimrc zprofile emacs.d/init.el config/nvim"    # list of files/folders to symlink in homedir
 # files="vimrc zshrc tmux.conf ideavimrc zprofile doom.d" # 둠으로 교체
 
 ##########
