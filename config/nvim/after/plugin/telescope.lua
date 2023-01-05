06b9995f0016b303c7fc4f1f38c5d5e75c8a6100
Sungho Kim <shirenbeat@gmail.com>
2023-01-06 03:20:18 +0900

    nvim: lsp

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,13 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
+vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
+
+
+vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
