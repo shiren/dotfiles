5962d9fc93f776a701b34dd117448716aa028be5
Sungho Kim <shirenbeat@gmail.com>
2023-01-15 15:44:17 +0900

    nvim: remove sensible

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -6,4 +6,4 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils()
+-- ColorMyPencils()
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -6,7 +6,6 @@ vim.cmd([[packadd packer.nvim]])
 return require("packer").startup(function(use)
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
-	use("tpope/vim-sensible")
 
 	use({
 		"nvim-telescope/telescope.nvim",
