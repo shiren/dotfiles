39cb1c6d879b43d30a522840e01c70c219fd8727
Sungho Kim <shirenbeat@gmail.com>
2023-01-19 18:26:11 +0900

    nvim: tresitter indent turn on

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -51,4 +51,11 @@ require("nvim-treesitter.configs").setup({
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	autotag = {
+		enable = true,
+	},
 })
