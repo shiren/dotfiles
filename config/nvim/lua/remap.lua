c9aeb228a8029a0482ef3b0367ee95b6dbe20bde
shiren <shirenbeat@gmail.com>
2023-04-14 00:07:19 +0900

    nvim: remap :W as write

--- config/nvim/lua/remap.lua
+++ config/nvim/lua/remap.lua
@@ -7,3 +7,5 @@ vim.keymap.set({ "n", "i", "v" }, "<C-g>", "<Esc>", { noremap = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.api.nvim_create_user_command("W", ":write", {})
