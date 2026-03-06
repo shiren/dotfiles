7a7b3c73c7b9c067b8be03896eeda56729804957
Sungho Kim(shiren) <shirenbeat@gmail.com>
2026-03-06 16:19:56 +0900

    hammerspoon: control spoon loading by UUID disable list

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,7 +7,37 @@ local function loadSpoonSafe(name, fn)
 	end
 end
 
-loadSpoonSafe("wiggle", function()
+-- IOPlatformUUID 확인:
+-- ioreg -rd1 -c IOPlatformExpertDevice | awk -F\" '/IOPlatformUUID/{print $4}'
+local platformUUID = hs.execute("ioreg -rd1 -c IOPlatformExpertDevice | awk -F\\\" '/IOPlatformUUID/{print $4}'"):gsub("%s+$", "")
+local disableSpoonByUUID = {
+	["AC201633-05AE-58CA-BCB4-4AF5A94B955B"] = {
+		"BrowserRouter",
+	},
+}
+
+local function isSpoonEnabledOnThisMac(spoonName)
+	local disabledSpoonList = disableSpoonByUUID[platformUUID]
+	if disabledSpoonList == nil then
+		return true
+	end
+
+	for _, disabledSpoonName in ipairs(disabledSpoonList) do
+		if disabledSpoonName == spoonName then
+			return false
+		end
+	end
+
+	return true
+end
+
+local function loadConfiguredSpoon(spoonName, fn)
+	if isSpoonEnabledOnThisMac(spoonName) then
+		loadSpoonSafe(spoonName, fn)
+	end
+end
+
+loadConfiguredSpoon("wiggle", function()
 	hs.loadSpoon("wiggle")
 	spoon.wiggle:init()
 	spoon.wiggle:bindHotkeys({
@@ -15,17 +45,17 @@ loadSpoonSafe("wiggle", function()
 	})
 end)
 
-loadSpoonSafe("VolumeOSC", function()
+loadConfiguredSpoon("VolumeOSC", function()
 	hs.loadSpoon("VolumeOSC")
 	spoon.VolumeOSC:start()
 end)
 
-loadSpoonSafe("BrowserRouter", function()
+loadConfiguredSpoon("BrowserRouter", function()
 	hs.loadSpoon("BrowserRouter")
 	spoon.BrowserRouter:start()
 end)
 
-loadSpoonSafe("WowOnebuttonToggle", function()
+loadConfiguredSpoon("WowOnebuttonToggle", function()
 	hs.loadSpoon("WowOnebuttonToggle")
 	spoon.WowOnebuttonToggle:start()
 end)
